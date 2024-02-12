107cf73a2507d2d289ce56fed61b16b0e6a3e088
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-02-12 21:12:12 +0000

    CRUD for director and for movies

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,10 +1,62 @@
 class DirectorsController < ApplicationController
+
+  # CRUD
+  #--------------------------------------------------
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
 
     render({ :template => "director_templates/index" })
   end
+
+  #--------------------------------------------------
+
+  def create
+
+    @new_director = Director.new
+    @new_director.name = params["query_name"]
+    @new_director.dob = params["query_dob"]
+    @new_director.bio = params["query_bio"]
+    @new_director.image = params["query_image"]
+
+    @new_director.save
+    redirect_to("/directors", { :notice => "Course created successfully." })
+
+
+  end # Of method.
+
+  #--------------------------------------------------
+
+  def update
+
+    the_id = params.fetch("path_id")
+    @director = Director.where({ :id => the_id }).at(0)
+
+    @director.name = params["query_name"]
+    @director.dob = params["query_dob"]
+    @director.bio = params["query_bio"]
+    @director.image = params["query_image"]
+
+    @director.save
+    redirect_to("/directors/#{the_id}", { :notice => "Director updated successfully."} )
+
+  end # Of method.
+
+  #--------------------------------------------------
+
+  def destroy
+
+    the_id = params["path_id"]
+    @director = Director.where({ :id => the_id }).at(0)
+
+    @director.destroy
+
+    redirect_to("/directors", { :notice => "Director deleted successfully."} )
+
+  end # Of method.
+
+  #--------------------------------------------------
+  #--------------------------------------------------
 
   def show
     the_id = params.fetch("path_id")
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,67 @@
 class MoviesController < ApplicationController
+
+  # CRUD
+  #--------------------------------------------------
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
 
     render({ :template => "movie_templates/index" })
   end
+
+  #--------------------------------------------------
+
+  def create
+
+    @new_movie = Movie.new
+    @new_movie.title = params["query_title"]
+    @new_movie.year = params["query_year"]
+    @new_movie.duration = params["query_duration"]
+    @new_movie.description = params["query_description"]
+    @new_movie.image = params["query_image"]
+
+
+    @new_movie.save
+    redirect_to("/movies", { :notice => "Movie created successfully." })
+
+
+  end # Of method.
+
+  #--------------------------------------------------
+
+  def update
+
+    the_id = params.fetch("path_id")
+    @movie = Movie.where({ :id => the_id }).at(0)
+
+    @movie.title = params["query_title"]
+    @movie.year = params["query_year"]
+    @movie.duration = params["query_duration"]
+    @movie.description = params["query_description"]
+    @movie.image = params["query_image"]
+
+    @movie.save
+    redirect_to("/movie/#{the_id}", { :notice => "Movie updated successfully."} )
+
+  end # Of method.
+
+  #--------------------------------------------------
+
+  def destroy
+
+    the_id = params["path_id"]
+    @movie = Movie.where({ :id => the_id }).at(0)
+
+    @movie.destroy
+
+    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+
+  end # Of method.
+
+  #--------------------------------------------------
+  #--------------------------------------------------
+
 
   def show
     the_id = params.fetch("path_id")
